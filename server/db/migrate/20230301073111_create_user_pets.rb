9df9df9f1b58e1658a222b9e7e33fb58b627949a
mosesochacha <ochachamoses443@@gmail.com>
2023-03-01 16:26:23 +0300

    changed ruby version

--- server/db/migrate/20230301073111_create_user_pets.rb
+++ server/db/migrate/20230301073111_create_user_pets.rb
@@ -1,5 +1,6 @@
 class CreateUserPets < ActiveRecord::Migration[6.1]
   def change
+    # creating table 
     create_table :user_pets do |t|
       t.references :user, foreign_key: true
       t.references :pet, foreign_key: true
