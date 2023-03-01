df71cbeb4a1c1b30c657168d56498fc9b7d7d385
mosesochacha <ochachamoses443@@gmail.com>
2023-03-01 11:31:38 +0300

    Created seed file sample data

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,5 +1,27 @@
+require 'faker'
 puts "🌱 Seeding spices..."
 
-# Seed your database here
+5.times do
+  user = User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    location: Faker::Address.city
+  )
 
-puts "✅ Done seeding!"
+  rand(1..3).times do
+    pet = Pet.create(
+      name: Faker::Creature::Animal.name,
+      age: rand(1..10),
+      breed: Faker::Creature::Dog.breed,
+      species: Faker::Creature::Animal.name,
+      gender: ['Male', 'Female'].sample,
+      image: Faker::LoremPixel.image(size: '300x300', category: 'animals'),
+      description: Faker::Lorem.sentence
+    )
+
+    user.pets << pet
+  end
+end
+
+puts "✅ Done seeding"
