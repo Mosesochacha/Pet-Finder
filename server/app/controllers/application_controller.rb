89156689edd18af1f308e8f86c906df7911fa08d
mosesochacha <ochachamoses443@@gmail.com>
2023-03-01 12:18:02 +0300

    Added get root for all pets and search

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -2,8 +2,19 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/pets' do
+    pet = Pet.all 
+    pet.to_json
   end
+   
+  #search
+  get '/pets/search/:name' do
+    pet = Pet.where("name LIKE ?", "%#{params[:name]}%")
+  pet.to_json 
+end
 
+get '/pets/searching/:breed' do
+  pet = Pet.where("breed LIKE ?", "%#{params[:breed]}%")
+  pet.to_json 
+end
 end
