31a90224e679bada4b08e88c7984b8bb41584302
mosesochacha <ochachamoses443@@gmail.com>
2023-03-02 02:00:58 +0300

    Added roots for getting pets for current user

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -1,6 +1,8 @@
 require 'faker'
 puts "🌱 Seeding spices..."
 
+animal_terms = ['cat', 'dog', 'rabbit', 'hamster', 'bird']
+
 5.times do
   user = User.create(
     name: Faker::Name.name,
@@ -9,14 +11,14 @@ puts "🌱 Seeding spices..."
     location: Faker::Address.city
   )
 
-  rand(1..3).times do
+  rand(1..3).times do |i|
     pet = Pet.create(
       name: Faker::Creature::Animal.name,
       age: rand(1..10),
       breed: Faker::Creature::Dog.breed,
       species: Faker::Creature::Animal.name,
       gender: ['Male', 'Female'].sample,
-      image: Faker::LoremFlickr.image(size: '300x300', search_terms: ['animals']),
+      image: Faker::LoremFlickr.image(size: '300x300', search_terms: [animal_terms[i % animal_terms.length]]),
       description: Faker::Lorem.sentence
     )
 
