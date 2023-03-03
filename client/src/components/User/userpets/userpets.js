7e782fd34f4cb8730baa7e6c5de55324ddc88028
mosesochacha <ochachamoses443@@gmail.com>
2023-03-04 02:18:42 +0300

    Added alerts when no pet found for usr

--- client/src/components/User/userpets/userpets.js
+++ client/src/components/User/userpets/userpets.js
@@ -4,23 +4,35 @@ import React, { useState, useEffect } from "react";
 
 export default function UserPet() {
   const [loading, setLoading] = useState(true);
-  const [pets, setPets] = useState([]);
+  const [pets, setPets] = useState(null);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     axios
       .get("https://pet-finder-9j4w.onrender.com/pets/user")
       .then((resp) => {
-        setPets(resp.data);
+        setPets(resp.data.pets);
         setLoading(false);
+        alert("Pets loaded successfully!");
       })
       .catch((err) => {
         console.error(err);
+        setError("Failed to fetch pets");
         setLoading(false);
+        alert("Failed to fetch pets!");
       });
   }, []);
 
   if (loading) {
     return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!pets || pets.length === 0) {
+    return <p>You have no pets.</p>;
   }
 
   return (
@@ -30,36 +42,29 @@ export default function UserPet() {
       </center>
 
       <div className="pets">
-        {Array.isArray(pets) &&
-          pets.map((newpets) => {
-            return (
-              <div key={newpets.id}>
-                <div className="card" style={{ width: "18rem" }}>
-                  <img
-                    src={newpets.image}
-                    className="card-img-top"
-                    alt="..."
-                  />
-                  <div className="card-body">
-                    <h3 className="card-title"> Name: {newpets.name}</h3>
-                    <h4 className="card-title">Species: {newpets.species}</h4>
-                    <p className="card-text">
-                      DESCRPTION: {newpets.description}
-                    </p>
-                    <div className="edits">
-                      <i className="material-icons">delete</i>
-                      <NavLink to="/edit">
-                        <i className="material-icons">edit</i>
-                      </NavLink>
-                    </div>
-                    <div className="edits mt-2">
-                      <h6 className="edit">Save</h6>
-                    </div>
+        {pets.map((newpets) => {
+          return (
+            <div key={newpets.id}>
+              <div className="card" style={{ width: "18rem" }}>
+                <img src={newpets.image} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h3 className="card-title"> Name: {newpets.name}</h3>
+                  <h4 className="card-title">Species: {newpets.species}</h4>
+                  <p className="card-text">DESCRPTION: {newpets.description}</p>
+                  <div className="edits">
+                    <i className="material-icons">delete</i>
+                    <NavLink to="/edit">
+                      <i className="material-icons">edit</i>
+                    </NavLink>
+                  </div>
+                  <div className="edits mt-2">
+                    <h6 className="edit">Save</h6>
                   </div>
                 </div>
               </div>
-            );
-          })}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
