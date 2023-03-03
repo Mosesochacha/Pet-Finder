18cc4c24473edfa775094efcd5e33829d45c8fad
mosesochacha <ochachamoses443@@gmail.com>
2023-03-03 08:24:27 +0300

    Added lougout functionality

---
+++ client/src/components/Authautication/logout.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+export default function Logout() {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    const res = await fetch("https://pet-finder-9j4w.onrender.com/user/logout", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    if (data.message) {
+      setMessage(data.message);
+      setError("");
+    } else {
+      setMessage("");
+      setError(data.error);
+    }
+  };
+  return (
+    <div>
+      <button onClick={handleLogout}>LOGOUT</button>
+      <div>{message && <p>{message}</p>}</div>
+      <div>{error && <p>{error}</p>}</div>
+    </div>
+  );
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,20 @@
 
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// import Login from './components/Authautication/login';
+import Logout from './components/Authautication/logout';
 //  import App from './App/App';
 //  import Petdelete from './components/Edits/delete';
 // import Login from './components/Authautication/login';
 // import Register from './components/Authautication/register';
-import AddPet from './components/Pets/addpets';
+// import AddPet from './components/Pets/addpets';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <Logout/>
     // <App />
     // <Register/>
     // <Login/>
-<AddPet/>
+// <AddPet/>  
 )
 
 // If you want to start measuring performance in your app, pass a function
