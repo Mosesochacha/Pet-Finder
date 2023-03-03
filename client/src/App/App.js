11b54f267144f20a9bc8471c6a1ad0c34c911904
mosesochacha <ochachamoses443@@gmail.com>
2023-03-04 02:00:05 +0300

    Added Routing

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -4,10 +4,10 @@ import AddPet from "../components/Pets/addpets";
 import { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import EditPet from "../components/Pets/edit";
-// import Login from "../components/User/login";
+ import Login from "../components/User/login";
  import SearchFunction from "../components/search/search";
 import Register from "../components/User/register";
-// import UserPet from "../components/User/userpets/userpets";
+ import UserPet from "../components/User/userpets/userpets";
 // import Homepage from "../components/homepage/homepage";
 import Navbar from "../Navbar";
 
@@ -19,13 +19,10 @@ function App() {
     <div className="Container">
       <Navbar setSearchText={setSearchText} setSearchType={setSearchType} /> 
       <Switch>
-        {/* <Route exact path="/login"> */}
-          {/* <Login />
+        <Route exact path="/login">
+           <Login />
         </Route>
-        <Route exact path="/register">
-          <Register />
-        </Route>
-        <div> */}
+      
         <Route exact path="/register">
           <Register />
         </Route>
@@ -38,9 +35,9 @@ function App() {
           <Route exact path="/edit">
             <EditPet />
           </Route>
-           {/* <Route exact path="/view"> */}
-            {/* <UserPet /> */}
-          {/* </Route>  */}
+           <Route exact path="/view"> 
+            <UserPet />
+           </Route> 
           <Route><SearchFunction searchtext={searchText} searchtype={searchType} /></Route> 
           
         {/* </div> */}
--- client/src/components/User/userpets/userpets.js
+++ client/src/components/User/userpets/userpets.js
@@ -1,56 +1,66 @@
-// import axios from "axios";
-// import { NavLink } from "react-router-dom";
-// import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
+export default function UserPet() {
+  const [loading, setLoading] = useState(true);
+  const [pets, setPets] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get("https://pet-finder-9j4w.onrender.com/pets/user")
+      .then((resp) => {
+        setPets(resp.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
+  }, []);
 
-// export default function UserPet({id}) {
-//   const [pets, setPets] = useState([]);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-//   useEffect(() => {
-//     axios.get("https://pet-finder-9j4w.onrender.com/pets/user").then((resp) => {
-//       setPets(resp.data);
-//     })
-//     .catch((err)=>{
-//       console.error(err)
-//     })
-//   }, []);
- 
+  return (
+    <div>
+      <center>
+        <h1 id="header">Adoptive, Foster happines</h1>
+      </center>
 
-//   return (
-//     <div>
-//       <center><h1 id="header">Adoptive, Foster happines</h1></center>
-
-//     <div className="pets">
-//       {pets.map((newpets)=>{
-//         return(
-//           <div>
-//           <div className="card" style={{ width: "18rem" }}>
-//             <img src={newpets.image} className="card-img-top" alt="..." />
-//             <div className="card-body">
-//               <h3 className="card-title"> Name: {newpets.name}</h3>
-//               <h4 className="card-title">Species: {newpets.species}</h4>
-//               <p className="card-text">DESCRPTION: {newpets.description}</p>
-//               {/* {message && <p>{message}</p>}
-//               {error && <p>{error}</p>}
-//      */}
-//               <div className="edits">
-//                 {/* <i onClick={handleDelete} className="material-icons"> */}
-//                   delete
-//                 {/* </i> */}
-//                 <NavLink to="/edit"></NavLink>
-//                 <i className="material-icons">edit</i>
-//               </div>
-//               <div className="edits mt-2">
-//                 <h6 className="edit">Save</h6>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         )
-//       })}
-//     </div>
-//     </div>
-   
-//   );
-// }
+      <div className="pets">
+        {Array.isArray(pets) &&
+          pets.map((newpets) => {
+            return (
+              <div key={newpets.id}>
+                <div className="card" style={{ width: "18rem" }}>
+                  <img
+                    src={newpets.image}
+                    className="card-img-top"
+                    alt="..."
+                  />
+                  <div className="card-body">
+                    <h3 className="card-title"> Name: {newpets.name}</h3>
+                    <h4 className="card-title">Species: {newpets.species}</h4>
+                    <p className="card-text">
+                      DESCRPTION: {newpets.description}
+                    </p>
+                    <div className="edits">
+                      <i className="material-icons">delete</i>
+                      <NavLink to="/edit">
+                        <i className="material-icons">edit</i>
+                      </NavLink>
+                    </div>
+                    <div className="edits mt-2">
+                      <h6 className="edit">Save</h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+}
