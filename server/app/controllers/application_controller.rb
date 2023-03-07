6a0e39cff1b323c541876de2605ea800494454dc
mosesochacha <ochachamoses443@@gmail.com>
2023-03-07 11:20:13 +0300

    SOLVED COR ERROR

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -4,29 +4,29 @@ class ApplicationController < Sinatra::Base
   
 
   # Add user
-  post "/user/register" do
-    begin
-      user = User.create(
-        name: params[:name],
-        email: params[:email],
-        location: params[:location],
-        password: params[:password],
-        password_confirmation: params[:password_confirmation],
-      )
-      if user.valid?
-        session[:user_id] = user.id
-        { message: "User created successfully" }.to_json
-      else
-        { error: user.errors.full_messages }.to_json
+    post "/user/register" do
+      begin
+        user = User.create(
+          name: params[:name],
+          email: params[:email],
+          location: params[:location],
+          password: params[:password],
+          password_confirmation: params[:password_confirmation],
+        )
+        if user.valid?
+          session[:user_id] = user.id
+          { message: "User created successfully" }.to_json
+        else
+          { error: user.errors.full_messages }.to_json
+        end
+      rescue ActiveRecord::RecordNotUnique => e
+        { error: "Email address already registered" }.to_json
+      rescue ActiveRecord::RecordInvalid => e
+        { error: e.record.errors.full_messages.join(", ") }.to_json
+      rescue => e
+        { error: "Registration failed" }.to_json
       end
-    rescue ActiveRecord::RecordNotUnique => e
-      { error: "Email address already registered" }.to_json
-    rescue ActiveRecord::RecordInvalid => e
-      { error: e.record.errors.full_messages.join(", ") }.to_json
-    rescue => e
-      { error: "Registration failed" }.to_json
     end
-  end
 
   # Login
   post "/user/login" do
@@ -51,47 +51,46 @@ class ApplicationController < Sinatra::Base
   end
 
   
- # r add a new pet
- post "/add/pet/:id" do
-  user = User.find_by(id: params[:id])
-  if user
-    pet = Pet.new(
-      name: params[:name],
-      breed: params[:breed],
-      age: params[:age],
-      image: params[:image],
-      species: params[:species],
-      description: params[:description],
-      user_id: user.id
-    )
-    if pet.valid?
-      pet.save
+   # Add a new pet
+   post "/add/pet/:id" do
+    user = User.find_by(id: params[:id])
+    if user
+      pet = Pet.new(
+        name: params[:name],
+        breed: params[:breed],
+        age: params[:age],
+        image: params[:image],
+        species: params[:species],
+        description: params[:description],
+        user_id: user.id
+      )
+      if pet.valid?
+        pet.save
         status 201
         { message: "Pet added successfully" }.to_json
+      else
+        status 422
+        { error: pet.errors.full_messages }.to_json
+      end
     else
       status 422
-      { error: pet.errors.full_messages }.to_json
+      { error: "You must be logged in to add a pet" }.to_json
     end
-  else
-    status 422
-    { error: "You must be logged in to add a pet" }.to_json
   end
-else{
-  error: "failito add"
-}
-end
+
 
   # View all pets
   get "/pets" do
     pets = Pet.all
     pets.to_json
   end
+
     
-  # View all pets for current user
+# View all pets for current user
 get "/current/user/:id" do
-  pet = Pet.find_by(user_id: params[:id])
-  if pet
-    { message: "Here is your pet", pet: pet }.to_json
+  pets = Pet.where(user_id: params[:id])
+  if pets.any?
+    { message: "Here are your pets", pets: pets }.to_json
   else
     { message: "You haven't added any pets yet" }.to_json
   end
@@ -102,47 +101,58 @@ end
 
 
 
+
   # Search pets by name
   get "/pets/search/name/:name" do
     begin
-      pets = Pet.where("name LIKE ?", "#{params[:name]}%")
-      pets.to_json
+      name = params[:name]
+      raise "Name parameter missing" if name.nil? || name.empty?
+  
+      pets = Pet.where("name LIKE ?", "#{name}%")
+      if pets.empty?
+        status 404
+        { error: "No pets found with the given name" }.to_json
+      else
+        pets.to_json
+      end
     rescue => e
+      status 400
       { error: e.message }.to_json
     end
   end
+  
   
 
   # Search pets by breed
   get "/pets/search/breed/:breed" do
     begin
-      pets = Pet.where("breed LIKE ?", "#{params[:breed]}%")
-      pets.to_json
+      sanitized_breed = ActiveRecord::Base.sanitize_sql_like(params[:breed])
+      pets = Pet.where("LOWER(breed) LIKE LOWER(?)", "#{sanitized_breed}%")
+      if pets.empty?
+        halt 404, { error: "No pets found with the given breed" }.to_json
+      else
+        pets.to_json
+      end
     rescue StandardError => e
       { error: e.message }.to_json
     end
   end
+  
 
- # Update pet details
- put "/pets/update/:id" do
+# Update pet details
+put "/pets/update/:id" do
   begin
     pet = Pet.find_by(id: params[:id])
+    user = User.find(pet.user_id)
 
     if pet.nil?
       status 404
       return { error: "Pet not found" }.to_json
     end
 
-    unless pet.user_id == session[:user_id]
+    unless pet.user_id == user.id
       status 403
       return { error: "You are not authorized to update this pet" }.to_json
-    end
-
-    new_user_id = params[:new_user_id].to_i
-
-    if new_user_id.zero?
-      status 400
-      return { error: "Invalid new user ID provided" }.to_json
     end
 
     if !pet.update(
@@ -156,6 +166,7 @@ end
       status 422
       return { error: "Failed to update pet" }.to_json
     end
+
     { message: "Pet updated successfully" }.to_json
 
   rescue StandardError => e 
@@ -164,6 +175,9 @@ end
   end
 end
 
+
+
+# email
 get '/user/:email' do
   user = User.find_by(email: params[:email])
   user.to_json
@@ -175,20 +189,27 @@ get '/users' do
   users.to_json
 end
 
-# Delete pet
 delete "/pets/delete/:id" do
   begin
-    pet = Pet.find(params[:id])
-    if pet.user_id == user.id
+    pet = Pet.find_by(id: params[:id])
+    current_user = User.find(pet.user_id)
+
+    if pet.user_id == current_user.id
       pet.destroy
-      { message: "Pet deleted successfully" }.to_json
+      status 204
     else
+      status 403
       { error: "You are not authorized to delete this pet" }.to_json
     end
-  rescue StandardError => e 
+  rescue ActiveRecord::RecordNotFound => e
+    status 404
+    { error: e.message }.to_json
+  rescue => e 
+    status 500
     { error: e.message }.to_json
   end
 end
+
 
 
 
